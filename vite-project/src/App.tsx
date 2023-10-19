eac01b5aa061b2be17befd7ead4659055999dc71
Dobri <79987000+NehuaV@users.noreply.github.com>
2023-10-19 14:32:58 +0200

    fix configs

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { edenTreaty } from "@elysiajs/eden";
 import type { App } from "../../Elysia/src/index";
 import "./App.css";
