7004455779f30f9afd5e1b0f7fac35abcbcb88c7
Dobri <79987000+NehuaV@users.noreply.github.com>
2023-10-19 14:42:19 +0200

    add logging

--- Elysia/src/server.ts
+++ Elysia/src/server.ts
@@ -1,10 +1,12 @@
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
+import { logger } from "@bogeychan/elysia-logger";
 import users from "./handlers/users";
 import posts from "./handlers/posts";
 
 const app = new Elysia()
   .use(cors())
+  .use(logger())
   .get("/", () => "Hello Elysia")
   .post("/", () => "Post Elysia")
   .use(users)
